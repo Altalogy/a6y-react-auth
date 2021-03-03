4c8b774a8e7ab336efd0fae66e3337a57a9d48d2
Arkadiusz Celer <arku@altalogy.com>
2021-03-03 13:08:04 +0100

    added ErrorBoundary to SignIn and SignUp components

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import EmailPasswordForm from '../EmailPasswordForm'
 import FormLinks from '../FormLinks'
+import { ErrorBoundary } from '../UI'
 import './SignIn.css'
 
 export interface Props {
   className?: string
   onClick?: () => void
-  apiError?: unknown
+  apiError?: string
 }
 
 const SignIn = ({
   className = 'a6y-react-auth-sign-in-cmp',
   onClick,
+  apiError,
 }: Props): JSX.Element => {
   return (
     <div className={className}>
+      <ErrorBoundary showError={apiError ? true : false}>
+        {apiError}
+      </ErrorBoundary>
       <EmailPasswordForm onClick={onClick} />
       <FormLinks />
     </div>
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import EmailPasswordForm from '../EmailPasswordForm'
 import FormLinks from '../FormLinks'
+import { ErrorBoundary } from '../UI'
 import './SignUp.css'
 
 export interface Props {
   className?: string
   onClick?: () => void
-  apiError?: unknown
+  apiError?: string
 }
 
 const SignUp = ({
   className = 'a6y-react-auth-sign-up-cmp',
+  apiError,
 }: Props): JSX.Element => {
   return (
     <div className={className}>
+      <ErrorBoundary showError={apiError ? true : false}>
+        {apiError}
+      </ErrorBoundary>
       <EmailPasswordForm />
       <FormLinks />
     </div>
