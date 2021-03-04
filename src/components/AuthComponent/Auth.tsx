da5890d33c411024d7141af1518cb6ec627950aa
Arkadiusz Celer <arku@altalogy.com>
2021-03-04 11:29:14 +0100

    removed use effect

--- src/components/AuthComponent/Auth.tsx
+++ src/components/AuthComponent/Auth.tsx
@@ -1,23 +1,16 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import SignIn from '../../containers/SignInContainer'
 import SignUp from '../../containers/SignUpContainer'
 import ForgotPassword from '../../containers/ForgotPasswordContainer'
 
 export interface IAuthProps {
-  config: { [key: string]: string }
   className?: string
 }
 
 const AuthComponent = ({
-  config,
   className = 'a6y-react-auth',
 }: IAuthProps): JSX.Element => {
-  useEffect(() => {
-    if (config && config.currentForm) {
-      setCurrentForm(config.currentForm)
-    }
-  }, [])
-  const [currentForm, setCurrentForm] = useState('sign-in')
+  const [currentForm] = useState('sign-in')
   const getAuthForm = (): JSX.Element => {
     switch (currentForm) {
       case 'sign-up':
--- stories/components/AuthComponent.stories.tsx
+++ stories/components/AuthComponent.stories.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { Meta } from '@storybook/react'
 import Auth from '../../src/components/AuthComponent'
 
-export const AuthComponent: React.VFC<unknown> = args => (
-  <Auth config={{ currentForm: 'sign-in' }} {...args} />
-)
+export const AuthComponent: React.VFC<unknown> = args => <Auth {...args} />
 
 export default {
   title: 'Components/Auth',
