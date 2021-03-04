36f6abc31993fe405d7d42ed258ec32ecc4300d5
Arkadiusz Celer <arku@altalogy.com>
2021-03-04 12:42:30 +0100

    install aws-amplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,27 @@
-import Auth from './components/AuthComponent'
-export default Auth
+import React from 'react'
+import AuthCnt from './components/AuthComponent'
+import SignInCnt from './containers/SignInContainer'
+import SignUpCnt from './containers/SignUpContainer'
+import ForgotPasswordCnt from './containers/ForgotPasswordContainer'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import ForgotPassword from './components/ForgotPassword'
+
+const A6YReactAuth = {
+  // configure: {
+  //   provider: {
+  //     type: 'cognito',
+  //     userPoolId: '',
+  //     region: 'eu-central-1',
+  //   },
+  // },
+}
+
+export const Auth = (): JSX.Element => <AuthCnt />
+export const SignInContainer = (): JSX.Element => <SignInCnt />
+export const SignUpContainer = (): JSX.Element => <SignUpCnt />
+export const ForgotPasswordContainer = (): JSX.Element => <ForgotPasswordCnt />
+
+export { SignIn, SignUp, ForgotPassword }
+
+export default A6YReactAuth
