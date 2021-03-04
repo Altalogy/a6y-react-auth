49461afa9a07c518bac9283113797a9f63a09d17
Arkadiusz Celer <arku@altalogy.com>
2021-03-04 07:37:43 +0100

    DRY-ish improvement

--- src/components/FormLinks/FormLinks.tsx
+++ src/components/FormLinks/FormLinks.tsx
@@ -28,13 +28,16 @@ const FormLinks = ({
   path,
 }: IFormLinksProps): JSX.Element => {
   function renderLinks(): JSX.Element {
+    const ForgotPassword = (): JSX.Element => (
+      <Link className={className + '__link'} to='/forgot-password'>
+        Forgot Password
+      </Link>
+    )
     switch (path) {
       case 'sign-in':
         return (
           <>
-            <Link className={className + '__link'} to='/forgot-password'>
-              Forgot Password
-            </Link>
+            {ForgotPassword}
             <Link className={className + '__link'} to='/sign-up'>
               Sign Up
             </Link>
@@ -43,22 +46,14 @@ const FormLinks = ({
       case 'sign-up':
         return (
           <>
-            <Link className={className + '__link'} to='/forgot-password'>
-              Forgot Password
-            </Link>
+            {ForgotPassword}
             <Link className={className + '__link'} to='/sign-in'>
               Sign In
             </Link>
           </>
         )
       default:
-        return (
-          <>
-            <Link className={className + '__link'} to='/forgot-password'>
-              Forgot Password
-            </Link>
-          </>
-        )
+        return <>{ForgotPassword}</>
     }
   }
   return <div className={className + '__links'}>{renderLinks()}</div>
