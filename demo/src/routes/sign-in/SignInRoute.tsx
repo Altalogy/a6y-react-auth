40be9a922f95b61f25ff3054969e7cc68e77a931
Arkadiusz Celer <arku@altalogy.com>
2021-03-23 16:42:51 +0100

    fixed issues

--- demo/src/routes/sign-in/SignInRoute.tsx
+++ demo/src/routes/sign-in/SignInRoute.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import MainLayout from '../../layouts/MainLayout'
-import { SignInContainer } from 'a6y-react-auth'
+import { SignIn } from 'a6y-react-auth'
 
 const SignInRoute = () => {
   return (
     <MainLayout backNav={true}>
-      <SignInContainer onSuccess={(resp) => console.log('sign-in', resp)} />
+      <SignIn />
     </MainLayout>
   )
 }
